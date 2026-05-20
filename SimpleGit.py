a05de02b43963a0d9b472d241b588ebdf27ce2b1
Muhammed-Abdulbasit <muhammedabdulbasit2654@gmail.com>
2026-05-20 19:55:39 -0400

    Added imports, paths for read/write file and init repo function

---
+++ SimpleGit.py
@@ -0,0 +1,40 @@
+import argparse, collections, difflib, enum, hashlib, operator, os, stat
+import struct, sys, time, urllib.request, zlib
+
+
+# Data for one entry in the git index (.git/index)
+IndexEntry = collections.namedtuple('IndexEntry', [
+    'ctime_s', 'ctime_n', 'mtime_s', 'mtime_n', 'dev', 'ino', 'mode', 'uid',
+    'gid', 'size', 'sha1', 'flags', 'path',
+])
+
+
+class ObjectType(enum.Enum):
+    """Object type enum. There are other types too, but we don't need them.
+    See "enum object_type" in git's source (git/cache.h).
+    """
+    commit = 1
+    tree = 2
+    blob = 3
+
+
+def read_file(path):
+    """Read contents of file at given path as bytes."""
+    with open(path, 'rb') as f:
+        return f.read()
+
+
+def write_file(path, data):
+    """Write data bytes to file at given path."""
+    with open(path, 'wb') as f:
+        f.write(data)
+    
+def init(repo):
+    """Create directory for repo and initialize .git directory."""
+    os.mkdir(repo)
+    os.mkdir(os.path.join(repo, '.git'))
+    for name in ['objects', 'refs', 'refs/heads']:
+        os.mkdir(os.path.join(repo, '.git', name))
+    write_file(os.path.join(repo, '.git', 'HEAD'),
+               b'ref: refs/heads/master')
+    print('initialized empty repository: {}'.format(repo))
